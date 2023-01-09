19de618733b79c6db7a0019dce70194fd9f24696
Laurent Binder <laurentbinder@gmail.com>
2023-01-09 15:10:26 +0100

    add bootstrap, react-router-dom, sass

--- front_xscape/src/main.jsx
+++ front_xscape/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import bootstrap from "bootstrap";
+
+
+
+
 // import './custom.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter >
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
