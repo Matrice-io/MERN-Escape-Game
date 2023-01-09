3d7cf673c31969a25ea70f419631bce66c32e436
nelisa <ntl.elisa@gmail.com>
2023-01-09 14:14:42 +0100

    add booking models

--- back_xscape/models/bookings.js
+++ back_xscape/models/bookings.js
@@ -3,7 +3,7 @@ import { Schema, model } from 'mongoose';
 const bookingSchema = new Schema(
   {
     roomId : {type : Object, required : true},
-    date : {type : Object, required : true}
+    calendar : {type : Object, required : true}
   },
   { collection: "bookings" }
 );
