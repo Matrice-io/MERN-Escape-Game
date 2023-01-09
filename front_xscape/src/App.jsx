165b7bfb03f3e8c86a4a7229b4b212cf9d610111
nelisa <ntl.elisa@gmail.com>
2023-01-09 16:39:26 +0100

    add Sign Out page

--- front_xscape/src/App.jsx
+++ front_xscape/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import {Route,Routes} from "react-router-dom"
+
 import Navbar from './components/Navbar'
+import SignOut from "./pages/SignOut"
 
 function App() {
  
@@ -7,6 +10,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/signOut" element={<SignOut/>}/>
+      </Routes>
     </div>
   )
 }
