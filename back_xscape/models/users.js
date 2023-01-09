a3849fec90be494defd8c0bc5d46c0795f555df7
Laurent Binder <laurentbinder@gmail.com>
2023-01-09 10:36:16 +0100

    add users routes

--- back_xscape/models/users.js
+++ back_xscape/models/users.js
@@ -1,11 +1,16 @@
 import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    // birthdate ...
-    creationDate: {type: Date, required: true}
-}, {collection: "User"})
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    birthdate: { type: Date, required: true },
+    creationDate: { type: Date, required: true },
+  },
+  { collection: "users" }
+);
 
 const User = model('User', userSchema);
 
