20bcea1490747665b04f3e9c436d6a2ac7c6ba77
Laurent Binder <laurentbinder@gmail.com>
2023-01-09 10:17:18 +0100

    add routes to rooms

--- back_xscape/models/rooms.js
+++ back_xscape/models/rooms.js
@@ -3,13 +3,39 @@ import { Schema, model } from "mongoose";
 // we don't need the rooms model cause we don't need to create any new model yet
 
 const roomsSchema = new Schema(
-    {
-        name: {type: String, required: true},
-        creationDate: {type: Date, required: true}
-    },
-    { collection: "rooms" }
-)
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    capacity: { type: Number, required: true },
+    img: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    minplayers: { type: Number, required: true },
+
+    // creationDate: {type: Date, required: true}
+  },
+  { collection: "rooms" }
+);
 
 const Room = model("rooms", roomsSchema);
 
 export default Room;
+
+// 
+
+// _id
+// 63b7ff145d924d5569855310
+// name
+// "Ghost Buster"
+// age
+// 16
+// capacity
+// 10
+// img
+// "https://thechive.com/wp-content/uploads/2021/02/ghostbuster-cartoons-t…"
+// description
+// "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, exercit…"
+// price
+// 99
+// minplayers
+// 4
