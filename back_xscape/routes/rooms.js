506749de2c82142b7a679996efaa68d67404052f
nelisa <ntl.elisa@gmail.com>
2023-01-09 14:22:38 +0100

    add show one room

--- back_xscape/routes/rooms.js
+++ back_xscape/routes/rooms.js
@@ -10,6 +10,17 @@ const roomRoutes = (app) => {
         res.status(400).json({ err });
       });
   });
+
+  app.get("/rooms/:id", (req, res) => {
+    const roomId= req.params.id
+    Room.find({_id : roomId})
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) =>{
+        res.status(400).json(err)
+      })
+  })
 };
 
 export default roomRoutes;
