4bb332eaf133bfa7f4899053bf67071ae43e1d8f
HugoA <aragones.hugo@gmail.com>
2023-01-10 10:39:23 +0100

    Added React-Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import Header from "./components/Header"
 import { createTheme } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
@@ -6,6 +5,7 @@ import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import { Routes, Route } from 'react-router-dom'
 
 const theme = createTheme({
     palette: {
@@ -23,9 +23,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      {/* <SignIn /> */}
-      {/* <SignUp /> */}
-      <Home />
+      <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </ThemeProvider>
   )
